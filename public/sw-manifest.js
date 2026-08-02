cd262b0d3dc762db9bb96c486b23e503219e6560
David Berger <david.berger@dotag.ch>
2026-08-02 11:21:25 +0200

    manifest updated

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "7a92aa78899c15fd";
+self.__SHELL_BUILD = "70ba464fd59512dc";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-backup.css",
   "/css/admin/admin-home.css",
