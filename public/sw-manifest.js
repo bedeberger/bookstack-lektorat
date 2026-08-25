20796780ab7931640acf48c0b0df51037bd4174b
David Berger <bede.berger@gmail.com>
2026-08-25 10:22:07 +0000

    tab komponente

--- public/sw-manifest.js
+++ public/sw-manifest.js
@@ -4,7 +4,7 @@
 //
 // __SHELL_BUILD: content hash over all listed assets → drives SHELL_CACHE.
 // __SHELL_MANIFEST: the coherent set precached atomically at SW install.
-self.__SHELL_BUILD = "9d0c7731491d5e51";
+self.__SHELL_BUILD = "6723595316b9d189";
 self.__SHELL_MANIFEST = [
   "/css/admin/admin-backup.css",
   "/css/admin/admin-home.css",
